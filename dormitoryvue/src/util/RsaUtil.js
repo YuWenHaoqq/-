6fcbd115a659c7cb1bf5adb5070961a38cdb7e2b
余文豪 <1471689575@qq.com>
2019-12-25 16:41:58 +0800

    补

--- dormitoryvue/src/util/RsaUtil.js
+++ dormitoryvue/src/util/RsaUtil.js
@@ -1,30 +1,24 @@
-import JSEncrypt from 'jsencrypt'
-import {get} from "@/util/HttpUtil";
+import{get} from "@/util/HttpUtil";
 import {Message} from "element-ui";
+import NodeRsa from 'node-rsa'
 
-export function rsaEncryption(text) {
-    let encrypt=new JSEncrypt();
-    encrypt.setPublicKey(getRsaKey())
-    let ll=encrypt.encrypt(text)
+export function getRsaKey() {
+    get('/api/encryption/rsa').then(res=>{
+        window.console.log(res.code)
+        sessionStorage.setItem("rsa",res.data)
+    }).catch(function (err) {
+        Message.error(err.message)
+    })
 
-    let ff=new JSEncrypt()
-    ff.setPrivateKey('MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAMgRMi6GHUdxlI00NJfSnutFNR0TsmZasvGxUkUliHrVhIJEH3EppafB2SKRsVLfzm5RkLXtQBGlxMOl8KwVEs8lP3TIyUbOS8pnPxp7vVIlWC/ldBelfE2JIjbRs3+7kfiHe5xuNAzgwJjspN8ZafKER+M2WoseoFVAwBV/c8lHAgMBAAECgYBZ/RIbRwvIZNgyoza8y2nb3SdEyuVFkKpIeD/PBlDbH0sY5NdXqSnvyQuHtwyRgEVz7ZAvpGVbCrr0XaFhZaEta4wcGmBQhLmkKqEqrOaGNhJoFQQhIHEObRBFvCEfFJPvmWTrdo/MTJVXv3wODRHprrJXVy4O/x0PxutYrHe74QJBAOuj6mV8ZbuhfdBCr4hIA+y4pPK04QG9FThnM9YXbHq1Ej+wIOFlt9itvckKODOJ+H5rcSGFwsSAy9l+P0KY51kCQQDZWnHto819ONIMyZ6syorZzRUoT4wxnUp/4YdmVYMldpzVT+DQrDHfpFovbxy3/JOgxiTG0Lvww1W/mlosYsGfAkEAwfbpaKRfMXWbFKTvzGRLJr5OuKH/XOkB4K3C04Xwe4k0/kvCpQaPsMJDKItK+9GkRJu4QfnS+uFx6xOqiu1F8QJABM8VejaHk1+wdgv38MbCb7Mz5qJr7aUXQuEQh9kYf7myGp4b0UIeEF6oiCgjMIbsFUCNC8EOh3Mq8IRPvm+o7QJBAOGvKARRYP7fC7zej1M5YwC3xaEEUiSQfXGcNa9KJBuu5NCbJmnS/hOgUp9O6pJQQwqqPPg7zTR+oz1Xie0HTPY=')
-    window.console.log("加密",ll)
-    window.console.log("解密",ff.decrypt(ll))
-    return encrypt.encrypt(text);
 }
 
-//获得rsa密钥
-export function getRsaKey() {
-    if (sessionStorage.getItem("rsa")===null){
-        get('api/encryption/rsa').then(
-            res => {
-                sessionStorage.setItem("rsa",res.data)
-            }).catch(function () {
-            Message.error("请求错误,请刷新重试")
-        })
-    }
-    return sessionStorage.getItem("rsa")
-
-
+export function rsaEncryption(text,encoding="base64",source_encoding ='utf-8') {
+    let key=new NodeRsa()
+    let publicKey='MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDIETIuhh1HcZSNNDSX0p7rRTUdE7JmWrLxsVJFJYh61YSCRB9xKaWnwdkikbFS385uUZC17UARpcTDpfCsFRLPJT90yMlGzkvKZz8ae71SJVgv5XQXpXxNiSI20bN/u5H4h3ucbjQM4MCY7KTfGWnyhEfjNlqLHqBVQMAVf3PJRwIDAQAB'
+    key.setOptions({
+        encryptionScheme:"pkcs1"
+    })
+    key.importKey(publicKey,"pkcs8-public-pem")
+//    加密并返回加密结果
+    return key.encrypt(text,encoding,source_encoding)
 }
